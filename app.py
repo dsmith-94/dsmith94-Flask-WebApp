ffd8c1c2c7d0f91bf8bf6ef39d9c983389b220a6
Dawson Smith <dawson.smith2488@gmail.com>
2026-04-02 17:29:13 -0500

    updated my readme and updated my app.py

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World!!"
 
+
+@app.route("/Daw")
+def Daw():
+    x = 6
+    y = 15
+    z = x + y
+    name = "Daw"
+    return f"{name}, the sum of {x} and {y} is {z}"
+
+
 if __name__ == "__main__":
-      app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
