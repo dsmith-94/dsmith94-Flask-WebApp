9c3ec9f58f3a6b6c71a89ba96cc91464e70a0d68
Dawson Smith <dawson.smith2488@gmail.com>
2026-04-02 17:49:43 -0500

    created about.html and started doing jinja templates

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 # module 10 - Flask Application
 # Mike Colbert 10/28/2025
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    return render_template("index.html")
 
 
 @app.route("/Daw")
